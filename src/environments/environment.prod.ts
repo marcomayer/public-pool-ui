61da303866d2063b8381af40202a5771706741a7
Marco Mayer <marco.mayer@gmail.com>
2025-02-14 16:59:48 +0100

    ports

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
-    API_URL: 'https://public-pool.io:40557',
-    STRATUM_URL: 'public-pool.io:21496'
+  production: true,
+  API_URL: 'http://vm:3334',
+  STRATUM_URL: 'vm:3333',
 };
